b2805cc0c7d118fe8fd27941543e2b6523c4ca12
cvolpe12 <christopher.volpe12@gmail.com>
2019-01-23 11:22:47 -0500

    created class databases

--- db/migrate/20190123145403_create_users.rb
+++ db/migrate/20190123145403_create_users.rb
@@ -1,7 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.text name
-      t.integer age
+      t.text :name
+      t.integer :age
+    end
   end
 end
