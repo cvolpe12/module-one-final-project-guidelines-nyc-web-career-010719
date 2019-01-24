d2c4488ba3f014a5c3ab5801ac6edb3135e0fe21
powell956 <powell956@gmail.com>
2019-01-24 10:59:01 -0500

    necessary additios to search

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,8 @@
 require_relative '../config/environment'
 
+Person.find_by_name('Tom Cruise')
+Person.films(Person.find_by_name('Tom Cruise')['known_for'])
+
 welcome
 menu
 goodbye
---
+++ lib/person.rb
@@ -0,0 +1,33 @@
+class Person < ActiveRecord::Base
+  has_many :actor_movies
+  has_many :movies, through: :actor_movies
+
+  def self.find_by_name(person_name)
+    person_name = person_name.split(' ').join('+')
+    response_string = RestClient.get("https://api.themoviedb.org/3/search/person?api_key=#{ENV['API_KEY']}&query=#{person_name}")
+    response_hash = JSON.parse(response_string)
+    person_hash = response_hash["results"].find { |person| person["name"] == person_name }
+  end
+
+  def self.films(films)
+    films.map do |film|
+      response_film = RestClient.get(film)
+      film_hash = JSON.parse(response_film)
+    end
+  end
+
+  def get_movies_from_api(person_name)
+    films = find_by_name(person_name)["known_for"]
+    films(films)
+  end
+
+  def print_movies(films)
+    films.each { |film| puts film["title"] }
+  end
+
+  def show_person_movies(person)
+    films = get_person_movies_from_api(person)
+    print_movies(films)
+  end
+
+end
