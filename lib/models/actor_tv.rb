55ba24927162a65948393b04266526472f64fb31
powell956 <powell956@gmail.com>
2019-01-24 16:38:29 -0500

    fix age bug

--- lib/models/actor_tv.rb
+++
@@ -1,4 +0,0 @@
-class ActorMovie < ActiveRecord::Base
-  belongs_to :tv
-  belongs_to :actor
-end
--- lib/models/tv_show.rb
+++ lib/models/tv_show.rb
@@ -11,19 +11,6 @@ class TvShow < ActiveRecord::Base
       tv["title"] == name
     end
     found_tv
-    # tv.create(title:found_tv["title"], year_released:found_tv["release_date"], vote_average:found_tv["vote_average"], brief_description:found_tv["overview"][0..100] << "...")
-    # puts found_tv["title"]
-    # puts found_tv["release_date"]
-    # puts found_tv["vote_average"]
-    # puts found_tv["overview"][0..100] << "..."
   end
-
-  # def get_title(name)
-  #   response_string = RestClient.get("https://api.thetvdb.org/3/search/tv?api_key=#{ENV['API_KEY']}&query=#{name}")
-  #   tv_hash = JSON.parse(response_string)
-  #   found_tv = tv_hash["results"].find do |tv|
-  #     tv["title"] == name
-  #   end
-  # end
-
-end
+  
+end #end of class
