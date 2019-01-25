97a8bde83b3dde32b20d3e04ea50f1c62add09c2
cvolpe12 <christopher.volpe12@gmail.com>
2019-01-25 11:47:46 -0500

    cleaning up spacing for user class

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -1,15 +1,9 @@
-# require_relative '../../config/environment'
-# require 'pry'
-
 class User < ActiveRecord::Base
   has_many :watchlists
   has_many :movies, through: :watchlists
 
   def self.authenticate(user_name, password)
     where(user_name: user_name, password: password).first
-    # binding.pry
   end
 
 end
-# user = User.new()
-# binding.pry
